a9d70d3259067b03ada7a2af44160375fb643396
Aleksandar Petrovic <aca208@gmail.com>
2018-04-21 01:55:29 +0200

    Ajax call for data.json

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,11 +1,28 @@
-var test1 = "";
-var objectTest = {lastname: "Jans", firstname: "Hans"};
+function CarData(){
 
+    var jsonCarData = JsonParseCars();
 
-function Test(){
+    return jsonCarData;
+}
 
-    test1 = "Test if works: ";
-    document.getElementById("Testh1").innerHTML = test1 + "First name = " + objectTest.firstname + ", Last name = " + objectTest.lastname;
-    // return test1;
+// AJAX Call to the requrested data.json file
+function JsonParseCars(){
+
+    var jcar = null;
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "Scripts/data.json", false);
     
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            jcar = JSON.parse(this.responseText);
+        }
+    }
+    xhttp.send(null);
+
+    if(jcar == null)
+    console.log("it is null");
+
+    return jcar;
+
 }
