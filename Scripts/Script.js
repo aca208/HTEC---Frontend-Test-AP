cf72e27c143ec3b6a46382b0aa1495b7d86e2eaf
Aleksandar Petrovic <aca208@gmail.com>
2018-04-21 23:54:45 +0200

    Changes to ajax call api

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -1,25 +1,14 @@
-function CarData(){
-
-    var jsonCarData = CarApiCall();
-    
-    return jsonCarData;
-}
-
 // AJAX Call to the requrested data.json file
-function CarApiCall(){
-
-    var jcar = null;
+function CarApiCall(CallbackFunc, data){
 
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "Scripts/data.json", false);
+    xhttp.open("GET", "Scripts/data.json", true);
     
     xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200){
-            jcar = JSON.parse(this.responseText);
+            CallbackFunc(JSON.parse(this.response), data);
         }
     }
     xhttp.send(null);
-
-    return jcar;
 
 }
